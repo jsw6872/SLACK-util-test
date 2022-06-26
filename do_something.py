d3580ff414e3c1d9b341d9ffe95ac66a08e3456c
seong woo <jsw6872@gmail.com>
2022-06-26 22:00:13 +0900

    updage variable name

--- do_something.py
+++ do_something.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import slack_channel_noti
-import slack_dm_noti_test
+from slack_handler import slack_channel_noti
+from slack_handler import slack_dm_noti_test
 import inspect
 
 import config
 
 token = config.token
-channel = '#일반'
+# channel = '#일반'
+channel_id = config.channel_id
 user_id = config.user_id
 
 
 if __name__ == '__main__':
-    channel_response = slack_channel_noti.post_message(token,  channel, f'{inspect.getfile(inspect.currentframe())} 실행이 완료 됐습니다.')
+    channel_response = slack_channel_noti.post_message(token,  channel_id, f'{inspect.getfile(inspect.currentframe())} 실행이 완료 됐습니다.')
     dm_response = slack_dm_noti_test.post_message(token,  user_id, f'{inspect.getfile(inspect.currentframe())} 실행이 완료 됐습니다.')
     print(channel_response.text)
     print(channel_response.status_code)
