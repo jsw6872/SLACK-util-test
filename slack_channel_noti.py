a9a9d9a7d1b6594c7875b478225393ed978a07ed
seong woo <jsw6872@gmail.com>
2022-06-26 21:27:24 +0900

    change file name

--- slack_channel_noti.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# 채널, 보낼 메시지, 
-def post_message(token, channel, message):
-    response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": message}
-    )
-    return response
