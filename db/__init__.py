6377ccee16fd9509753e813545a942592c8cdd01
seong woo <jsw6872@gmail.com>
2022-06-28 17:23:43 +0900

    update db_connector

---
+++ db/__init__.py
@@ -0,0 +1,4 @@
+import config
+import insert_memberinfo, select_member_info, db_connector
+
+__all__ = ['config', 'insert_memberinfo', 'select_member_info', 'db_connector']
--- db/insert_memberinfo.py
+++ db/insert_memberinfo.py
@@ -1,17 +1,6 @@
 import pymysql
 import pymysql.cursors
 
-import config
-
-DB_ADDRESS = config.DB_ADDRESS
-DB_USER = config.DB_USER
-DB_PASSWORD = config.DB_PASSWORD
-
-def db_connecter():
-  conn = pymysql.connect(host=DB_ADDRESS, port=3306, user=DB_USER,password=DB_PASSWORD, 
-                         db='birh_noti_test', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
-  return conn
-
 def insert_member_info(conn):
     cursor = conn.cursor()
     sql = '''
